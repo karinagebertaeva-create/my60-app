85b407a25739165d2959d52f5167ff2505f52243
karinagebertaeva-create <karinagebertaeva@gmail.com>
2026-09-06 19:30:33 +0500

    Add service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="my60-v2",A=["./","./index.html","./manifest.json","./icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
